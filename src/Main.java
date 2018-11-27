2460ff3f904be1c60e080b18d1213344ac9b3609
erwinhenraat <e.henraat@ma-web.nl>
2018-11-27 14:49:33 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Person[] persons = {
+                new Person()
+        };
+
+        persons[0].setEyecolor("blue");
+        System.out.println(persons[0].getEyecolor());
+
+    }
+}
---
+++ src/Person.java
@@ -0,0 +1,12 @@
+public class Person {
+    private String eyecolor;
+    public Person(){
+
+    }
+    public void setEyecolor(String eyecolor){
+        this.eyecolor = eyecolor;
+    }
+    public String getEyecolor(){
+        return this.eyecolor;
+    }
+}
